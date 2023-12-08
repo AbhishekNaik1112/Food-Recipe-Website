98e90f5894b4aefe2e0d2f30d1e9b804ccb9d3d2
AbhishekNaik1112 <abhisek.naik@kalvium.community>
2023-12-08 09:58:08 +0530

    removed timer added comments

--- script.js
+++ script.js
@@ -2,35 +2,36 @@
 
 function getData() {
   const randommeal = document.getElementsByClassName(
-      "recipe-of-the-day-container"
+    "recipe-of-the-day-container"
   );
 
   const apiUrl = `https://www.themealdb.com/api/json/v1/1/random.php`;
 
   fetch(apiUrl)
-      .then((response) => response.json())
-      .then((data) => {
-          const result = data.meals[0];
-          console.log(result);
+    .then((response) => response.json())
+    .then((data) => {
+      //fetching the random dish using the random api
+      const result = data.meals[0];
+      console.log(result);
 
-          randommeal[0].innerHTML = `
+      randommeal[0].innerHTML = `
             <h2>${result.strMeal}</h2>
             <img id="meal-img" src="${result.strMealThumb}" alt="${result.strMeal}">
         `;
 
-          randommeal[0].recipeDetails = {
-              meal: result.strMeal,
-              ingredients: getIngredients(result),
-              instructions: result.strInstructions,
-          };
+      randommeal[0].recipeDetails = {
+        meal: result.strMeal,
+        ingredients: getIngredients(result),
+        instructions: result.strInstructions,
+      };
 
-          randommeal[0].onclick = displayRecipeModal;
-      });
+      randommeal[0].onclick = displayRecipeModal;
+    });
 }
 
 function displayRecipeModal() {
   const recipeDetails = document.getElementsByClassName(
-      "recipe-of-the-day-container"
+    "recipe-of-the-day-container" //displaying a popup modal which will contain the measure ingredients and instructions.
   )[0].recipeDetails;
 
   const modalContent = `
@@ -38,23 +39,24 @@ function displayRecipeModal() {
     <h3>Ingredients:</h3><br>
     ${recipeDetails.ingredients}
     <h3>Instructions:</h3>
-    <p>${recipeDetails.instructions}</p>
-`;
+    <p>${recipeDetails.instructions}</p>                       
+`; // grabs the instuctions
 
   Swal.fire({
-      html: modalContent,
-      confirmButtonText: "Close",
+    html: modalContent,
+    confirmButtonText: "Close", //used the sweetalert cdn to implement the modal that is linked in the head of the html.
   });
 }
 
 function getIngredients(result) {
   const ingredients = [];
   for (let i = 1; i <= 20; i++) {
-      const ingredient = result[`strIngredient${i}`];
-      const measure = result[`strMeasure${i}`];
-      if (ingredient && measure) {
-          ingredients.push(`${measure} ${ingredient}<br>`);
-      }
+    //as there are 20 measure and ingredients it will loop through 0<i<20 to gather all the ingredients and its measure
+    const ingredient = result[`strIngredient${i}`];
+    const measure = result[`strMeasure${i}`];
+    if (ingredient && measure) {
+      ingredients.push(`${measure} ${ingredient}<br>`);
+    }
   }
   return ingredients.join("");
 }
@@ -64,68 +66,21 @@ getData();
 //Dark Mode logic-styles and layout will be changed in Milestone 3
 const darkModeBtn = document.getElementById("dark-mode-btn");
 darkModeBtn.addEventListener("click", () => {
-  document.body.classList.toggle("dark-mode");
+  document.body.classList.toggle("dark-mode"); //dark mode button self explanatory
 });
 
 //scroll to top logic-styles and layout will be changed in Milestone 3
 document.getElementById("scroll-top-btn").addEventListener("click", () => {
   window.scrollTo({
-      top: 0,
-      behavior: "smooth",
+    top: 0,
+    behavior: "smooth", //scroll to top self explanatory
   });
 });
-
-//timer logic-styles and layout will be changed in Milestone 3
-const timerContainer = document.getElementById("active-timers");
-document.getElementById("timer-start-btn").addEventListener("click", () => {
-  let minutes = parseInt(document.getElementById("timer-input").value);
-  if (isNaN(minutes) || minutes <= 0) {
-      alert("Please enter a valid number of minutes.");
-      return;
-  }
-
-  createTimer(minutes);
-});
-
-function createTimer(minutes) {
-  const endTime = new Date(new Date().getTime() + minutes * 60000);
-  const timerItem = document.createElement("li");
-  const endTimeSpan = document.createElement("span");
-  const countdownSpan = document.createElement("span");
-  const removeBtn = document.createElement("button");
-
-  removeBtn.innerText = "Remove";
-  removeBtn.onclick = () => {
-      clearInterval(timerItem.intervalId);
-      timerItem.remove();
-  };
-
-  endTimeSpan.innerText = `Ends at ${endTime.toLocaleTimeString()} `;
-  countdownSpan.innerText = `${minutes}m 00s remaining`;
-
-  timerItem.appendChild(endTimeSpan);
-  timerItem.appendChild(countdownSpan);
-  timerItem.appendChild(removeBtn);
-  timerContainer.appendChild(timerItem);
-
-  timerItem.intervalId = setInterval(() => {
-      const now = new Date();
-      const timeLeft = endTime.getTime() - now.getTime();
-
-      if (timeLeft <= 0) {
-          clearInterval(timerItem.intervalId);
-          countdownSpan.innerText = `Time's up!`;
-      } else {
-          const minutesLeft = Math.floor(timeLeft / 60000);
-          const secondsLeft = Math.floor((timeLeft % 60000) / 1000);
-          countdownSpan.innerText = `${minutesLeft}m ${secondsLeft}s remaining`;
-      }
-  }, 1000);
-}
 
 //more button logic-styles and layout will be changed in Milestone 3
 const morebtn = document.getElementById("get-more-btn");
 morebtn.addEventListener("click", () => {
+  //this button will get a new random dish
   getData();
 });
 
@@ -135,15 +90,14 @@ const whatsappButton = document.getElementsByClassName("return-to-home-btn")[0];
 
 whatsappButton.addEventListener("click", () => {
   shareOnWhatsApp();
-});
-
+}); //event listener for whatsapp share with text
 
 function shareOnWhatsApp() {
   const textToShare =
-      "This site is a great place to look for recipes.Give it a try." +
-      `https://abhisheknaik1112.github.io/Food-Recipe-Website/`;
+    "This site is a great place to look for recipes.Give it a try." +
+    `https://abhisheknaik1112.github.io/Food-Recipe-Website/`;
   window.open(
-      "https://api.whatsapp.com/send?text=" + encodeURIComponent(textToShare)
+    "https://api.whatsapp.com/send?text=" + encodeURIComponent(textToShare)
   );
 }
 
@@ -152,15 +106,15 @@ function getCategoryData(key) {
   let categoryapi = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${key}`;
 
   fetch(categoryapi)
-      .then((response) => response.json())
-      .then((result) => {
-          const meals = result.meals;
-          console.log(result);
-          displayMeals(meals);
-      })
-      .catch((err) => {
-          console.log("Error fetching meal details:", err.message);
-      });
+    .then((response) => response.json()) //fetching the dishes of the entered category
+    .then((result) => {
+      const meals = result.meals;
+      console.log(result);
+      displayMeals(meals);
+    })
+    .catch((err) => {
+      console.log("Error fetching meal details:", err.message);
+    });
 }
 
 function displayMeals(meals) {
@@ -168,75 +122,78 @@ function displayMeals(meals) {
   mealsList.innerHTML = "";
 
   meals.forEach((meal) => {
-      const mealCard = document.createElement("div");
-      mealCard.classList.add("meal-card");
+    const mealCard = document.createElement("div");
+    mealCard.classList.add("meal-card");
 
-      const mealImage = document.createElement("img");
-      mealImage.src = meal.strMealThumb;
-      mealImage.alt = meal.strMeal;
+    const mealImage = document.createElement("img");
+    mealImage.src = meal.strMealThumb;
+    mealImage.alt = meal.strMeal; //creating divs with image and name for the searched category
 
-      const mealName = document.createElement("h3");
-      mealName.textContent = meal.strMeal;
+    const mealName = document.createElement("h3");
+    mealName.textContent = meal.strMeal; //saving this name so that when a dish image is clicked it will be passed in the api by name and will gather the data of that specific dish
 
-      mealCard.appendChild(mealImage);
-      mealCard.appendChild(mealName);
+    mealCard.appendChild(mealImage);
+    mealCard.appendChild(mealName);
 
-      mealsList.appendChild(mealCard);
+    mealsList.appendChild(mealCard);
 
-      mealCard.addEventListener("click", function() {
-          getMealDetails(meal.strMeal);
-      });
+    mealCard.addEventListener("click", function () {
+      getMealDetails(meal.strMeal);
+    });
   });
 }
 
 function getMealDetails(mealName) {
-  let detailsApi = `https://www.themealdb.com/api/json/v1/1/search.php?s=${mealName}`;
+  let detailsApi = `https://www.themealdb.com/api/json/v1/1/search.php?s=${mealName}`; //the saved name in line 133 is passed here and the details of that dish is fetched.
 
   fetch(detailsApi)
-      .then((response) => response.json())
-      .then((result) => {
-          const mealDetails = result.meals[0];
+    .then((response) => response.json())
+    .then((result) => {
+      const mealDetails = result.meals[0];
 
-          const ingredients = [];
-          for (let i = 1; i <= 20; i++) {
-              const ingredient = mealDetails[`strIngredient${i}`];
-              const measure = mealDetails[`strMeasure${i}`];
-              if (ingredient && measure) {
-                  ingredients.push(`${measure} ${ingredient}`);
-              }
-          }
+      const ingredients = [];
+      for (let i = 1; i <= 20; i++) {
+        const ingredient = mealDetails[`strIngredient${i}`];
+        const measure = mealDetails[`strMeasure${i}`];
+        if (ingredient && measure) {
+          // //as there are 20 measure and ingredients it will loop through 0<i<20 to gather all the ingredients and its measure
+          ingredients.push(`${measure} ${ingredient}`);
+        }
+      }
 
-          Swal.fire({
-              title: mealDetails.strMeal,
-              html: `<p><h3>Ingredients:</h3>${ingredients.join(
-      "<br>"
-    )}</p><p><h3>Instructions:</h3>${mealDetails.strInstructions}</p>`,
-              confirmButtonText: "Close",
-          });
-      })
-      .catch((err) => {
-          console.log("Error fetching meal details:", err.message);
+      Swal.fire({
+        title: mealDetails.strMeal,
+        //paste the commented text in line 198 here and image will be shown for the search food when you open the popup
+        html: `<p><h3>Ingredients:</h3>${ingredients.join(
+          "<br>"
+        )}</p><p><h3>Instructions:</h3>${mealDetails.strInstructions}</p>`,
+        confirmButtonText: "Close",
       });
+    })
+    .catch((err) => {
+      console.log("Error fetching meal details:", err.message);
+    });
 }
-
-document.addEventListener("DOMContentLoaded", function() {
+//giving a eventlistener i.e enter button so that when a category name is entered it will be passed in the category fetch
+document.addEventListener("DOMContentLoaded", function () {
   const searchbtn = document.getElementById("search");
   const searchvalue = document.getElementById("search-term");
 
-  searchbtn.addEventListener("click", function() {
+  searchbtn.addEventListener("click", function () {
+    var search = searchvalue.value;
+    console.log(search);
+    getCategoryData(search);
+  });
+
+  searchvalue.addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
       var search = searchvalue.value;
       console.log(search);
       getCategoryData(search);
-  });
-
-  searchvalue.addEventListener("keypress", function(event) {
-      if (event.key === "Enter") {
-          var search = searchvalue.value;
-          console.log(search);
-          getCategoryData(search);
-      }
+    }
   });
 });
 
+//If needed can generate the image for popup with this
 // imageUrl: mealDetails.strMealThumb,
 //imageAlt: mealDetails.strMeal,
