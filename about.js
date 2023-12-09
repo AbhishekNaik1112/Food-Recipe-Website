e234b8f142344342d21a46f60a7514663279b5a5
AbhishekNaik1112 <abhisek.naik@kalvium.community>
2023-12-09 11:50:50 +0530

    all errors removed

---
+++ about.js
@@ -0,0 +1,10 @@
+//new js file to remove reference error in about page
+
+const darkModeBtn = document.getElementById("dark-mode-btn");
+const appContainer = document.getElementById("app-container");
+
+darkModeBtn.addEventListener("click", () => {
+  document.body.classList.toggle("dark-mode");
+  appContainer.classList.toggle("dark-mode");
+});
+//dark mode logic
